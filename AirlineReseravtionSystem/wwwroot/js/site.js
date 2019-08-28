a30ccf2ff7e34f35f518990d6531707b057e3bf1
smishr06 <smishr06@LCS-215-052>
2019-08-28 17:41:01 -0400

    Passenger info Input and Checkbox selection added

--- AirlineReseravtionSystem/wwwroot/js/site.js
+++ AirlineReseravtionSystem/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// ----< Performing validation on the number of seats selcted >------
+
+function ValidatePetSelection(numberOfTickets) {
+    var checkboxes = document.getElementsByName("seatSelection");
+    var CheckedItems = 0;
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked)
+            CheckedItems++;
+    }
+    if (CheckedItems > numberOfTickets) {
+        alert("Selection Exceeding the Number of tickets");
+        return false;
+    }
+}  
